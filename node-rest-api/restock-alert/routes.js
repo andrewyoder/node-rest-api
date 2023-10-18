852fbdb378d82b9397b6da34f644d2ab39c82302
Andrew Yoder <andrwydr@aol.com>
2023-10-18 11:44:24 -0700

    fix routes

--- node-rest-api/restock-alert/routes.js
+++ node-rest-api/restock-alert/routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const RestockAlertController = require('./controllers/RestockAlertController');
 
-router.post("/addRestockAlert", RestockAlertController.addRestockAlert);
+router.post("/", RestockAlertController.addRestockAlert);
 
 module.exports = router;
