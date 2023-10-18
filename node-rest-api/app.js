19b5df0beb761c0abfa452bd07b4f495453a3052
Andrew Yoder <andrwydr@aol.com>
2023-10-18 10:49:52 -0700

    update response headers

--- node-rest-api/app.js
+++ node-rest-api/app.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+
 const sequelize = new Sequelize({
     dialect: "sqlite",
     //storage: "./TBD.db"
