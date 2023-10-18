b85e89204a65563b922512166d65945304ab8eb1
Andrew Yoder <andrwydr@aol.com>
2023-10-18 12:24:59 -0700

    move app.use declarations

--- node-rest-api/app.js
+++ node-rest-api/app.js
@@ -4,16 +4,9 @@ const cors = require('cors');
 const Sequelize = require('sequelize');
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-// Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
-    next();
-});
+
+
 
 const sequelize = new Sequelize({
     dialect: "sqlite",
@@ -27,6 +20,17 @@ RestockAlertModel.initialize(sequelize);
 sequelize
     .sync()
     .then(() => {
+        app.use(express.json());
+        app.use(cors());
+
+        // Add Access Control Allow Origin headers
+        app.use((req, res, next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Headers", "*");
+            res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+            next();
+        });
+
         app.use("/status", config.StatusRoutes);
         app.use("/restockAlert", config.RestockAlertRoutes);
 
