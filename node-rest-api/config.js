dcbdb3c4bbd4be00601b70b3bb5bbb91db0a11ad
Andrew Yoder <andrwydr@aol.com>
2023-11-15 17:55:34 -0800

    update port number

--- node-rest-api/config.js
+++ node-rest-api/config.js
@@ -1,4 +1,4 @@
-const api_port = process.env.PORT || 8000;
+const api_port = process.env.PORT || 443;
 
 const StatusRoutes = require('./status/routes');
 const RestockAlertRoutes = require('./restock-alert/routes');
