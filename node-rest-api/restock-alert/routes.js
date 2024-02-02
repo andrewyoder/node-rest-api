cc2d86ca55b20e91637a274ac4dd87e763e0d903
Andrew Yoder <andrwydr@aol.com>
2024-02-02 11:45:21 -0800

    change back to "get"

--- node-rest-api/restock-alert/routes.js
+++ node-rest-api/restock-alert/routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const RestockAlertController = require('./controllers/RestockAlertController');
 
-router.post("/", RestockAlertController.addRestockAlert);
+router.get("/", RestockAlertController.addRestockAlert);
 
 module.exports = router;
