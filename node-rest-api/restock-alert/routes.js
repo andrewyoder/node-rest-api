ca9acb36fef2c9e9d83234a3ad57e476e76a94cf
Andrew Yoder <andrwydr@aol.com>
2024-02-03 19:33:44 -0800

    fix update route

--- node-rest-api/restock-alert/routes.js
+++ node-rest-api/restock-alert/routes.js
@@ -5,6 +5,6 @@ const RestockAlertController = require('./controllers/RestockAlertController');
 router.get("/add", RestockAlertController.addRestockAlert);
 //router.get("/get", RestockAlertController.getAllUnsentAlerts);
 router.get("/get", RestockAlertController.getRestockAlerts);
-router.get("/update/:product", RestockAlertController.updateRestockAlerts);
+router.get("/update", RestockAlertController.updateRestockAlerts);
 
 module.exports = router;
