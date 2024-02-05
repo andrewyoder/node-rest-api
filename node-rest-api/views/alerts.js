9190bf65acff9fc781e1cfd5f84ab87c0a8c0863
Andrew Yoder <andrwydr@aol.com>
2024-02-05 08:56:01 -0800

    try doing everything via js file

--- node-rest-api/views/alerts.js
+++ node-rest-api/views/alerts.js
@@ -1,20 +1,35 @@
-function getEmailsForm() {
-    var productName = document.getElementById('product_name');
+if (!customElements.get('product-form')) {
+    customElements.define('product-form', class ProductForm extends HTMLElement {
+        constructor() {
+            super();
 
-    console.log("getEmailsForm() - product: " + productName);
+            this.form = this.querySelector('form');
+            this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
+            this.submitButton = this.querySelector('[type="submit"]');
+        }
 
-    var request = new Request('/restockAlert/get', {
-        method: "POST",
-        body: {
-            product: productName
+        onSubmitHandler(evt) {
+            evt.preventDefault();
+
+            const formData = new FormData(this.form);
+            var reqBody = {
+                'product': `${formData.get("product-name")}`
+            };
+
+            const request = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Upgrade-Insecure-Requests': 0,
+                    'Accept': `application/javascript`,
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                body: JSON.stringify(reqBody)
+            };
+
+            var api_url = 'restockAlert/add';
+
+            fetch(api_url, request);
         }
     });
-
-    request.headers = {
-        'content-type': 'text/javascript'
-    };
-
-    console.log("request: " + request);
-
-    fetch(request);
 }
