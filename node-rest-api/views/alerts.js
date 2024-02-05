269c5aab0dba42696024dde2ee341162c186fca5
Andrew Yoder <andrwydr@aol.com>
2024-02-04 21:45:10 -0800

    load script correctly...

--- node-rest-api/views/alerts.js
+++ node-rest-api/views/alerts.js
@@ -1,12 +1,16 @@
 function getEmailsForm() {
     var productName = document.getElementById('product_name');
 
+    console.log("getEmailsForm() - product: " + productName);
+
     var request = new Request('/restockAlert/get', {
         method: "POST",
         body: {
             product: productName
         }
     });
+
+    console.log("request: " + request);
 
     fetch(request);
 }
